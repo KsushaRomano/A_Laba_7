5fbc83531e25149b139aba4136305ae76559f9ae
KsushaRomano <55737387+KsushaRomano@users.noreply.github.com>
2019-11-24 18:57:02 +0300

    Update Laba_7_2.cpp

--- Laba_7_2.cpp
+++ Laba_7_2.cpp
@@ -1,11 +1,14 @@
 ﻿#include <iostream>
 #include <string>
+#include <windows.h>
 
 using namespace std;
 
 int main()
 {
 	setlocale(LC_ALL, "Russian");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
 	int i;
 	string S;
 	cout << "Введите строку: ";
